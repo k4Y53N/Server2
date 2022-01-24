93eae0aacbf5985f6fd8ed9e4c9fee021f5c0923
k4Y53N <51146583+k4Y53N@users.noreply.github.com>
2022-01-24 15:31:43 +0800

    complete servo module 	modified:   unit/JetsonMotar.py

--- unit/JetsonMotar.py
+++ unit/JetsonMotar.py
@@ -1,28 +1,32 @@
 from .RepeatTimer import RepeatTimer
 from Jetson import GPIO
+from threading import Lock
 
 
-class ServeMo(RepeatTimer):
+class Servo(RepeatTimer):
     def __init__(self):
         RepeatTimer.__init__(self)
         GPIO.setmode(GPIO.BOARD)
         outs = [35, 36, 37, 38, 40]  # FL, FR, RL, RR, ANGEL
         GPIO.setup(outs, GPIO.OUT)
-        self.FRONT_LEFT = GPIO.PWM(35, 100)
-        self.FRONT_RIGHT = GPIO.PWM(36, 100)
-        self.REAR_LEFT = GPIO.PWM(37, 100)
-        self.REAR_RIGHT = GPIO.PWM(38, 100)
-        self.ANGEL = GPIO.PWM(40, 100)
+        self.WHEEL_DUTY_CYCLE = 100
+        self.ANGEL_DUTY_CYCLE = 100
+        self.FRONT_LEFT = GPIO.PWM(35, self.WHEEL_DUTY_CYCLE)
+        self.FRONT_RIGHT = GPIO.PWM(36, self.WHEEL_DUTY_CYCLE)
+        self.REAR_LEFT = GPIO.PWM(37, self.WHEEL_DUTY_CYCLE)
+        self.REAR_RIGHT = GPIO.PWM(38, self.WHEEL_DUTY_CYCLE)
+        self.ANGEL = GPIO.PWM(40, self.ANGEL_DUTY_CYCLE)
+        self.lock = Lock()
 
     def init_phase(self):
         self.FRONT_LEFT.start(0)
         self.FRONT_RIGHT.start(0)
         self.REAR_LEFT.start(0)
         self.REAR_RIGHT.start(0)
-        self.ANGEL.start(0)
+        self.ANGEL.start(50)
 
     def execute_phase(self):
-        pass
+        self.set(0, 90)
 
     def close_phase(self):
         self.FRONT_LEFT.stop()
@@ -33,5 +37,34 @@ class ServeMo(RepeatTimer):
         GPIO.cleanup()
 
     def set(self, r, theta):
+        if self.lock.locked():
+            return
+
+        with self.lock:
+            r = int(r % 1 * 100)
+            theta = int(theta % 180)
+            self.ANGEL.ChangeDutyCycle(theta)
+            self.FRONT_LEFT.ChangeDutyCycle(r)
+            self.FRONT_RIGHT.ChangeDutyCycle(r)
+            self.REAR_LEFT.ChangeDutyCycle(r)
+            self.REAR_RIGHT.ChangeDutyCycle(r)
+
         self.reset_time()
-        pass
+
+
+if __name__ == '__main__':
+    from .Monitor import Monitor
+    import time
+
+    m = Monitor()
+    s = Servo()
+    s.start()
+    m.start()
+    m.set_row_string(0, 'Servo start')
+    time.sleep(5)
+    m.set_row_string(1, 'Servo change')
+    s.set(0.8, 75)
+    time.sleep(2)
+    print('close')
+    m.close()
+    s.close()
