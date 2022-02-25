f04b7367e3f6f5e7e6683775fb04c672b109ede6
k4Y53N <ks93041@gmail.com>
2022-02-25 12:58:16 +0800

    refactor Camera.py

--- src/Camera.py
+++ src/Camera.py
@@ -1,11 +1,15 @@
 import cv2
-import logging
+import logging as log
 import numpy as np
 from base64 import b64encode
 from threading import Thread
+from .RepeatTimer import RepeatTimer
+from typing import Tuple
 
 _width = 1280
 _height = 720
+_ascii_w = 70
+_ascii_h = 35
 
 
 def gstreamer_pipeline(
@@ -36,6 +40,15 @@ def gstreamer_pipeline(
     )
 
 
+def encode_image_to_b64(image: np.ndarray, size: Tuple[int, int]) -> str:
+    if image.shape != size:
+        image = cv2.resize(image, size)
+    ret, jpg = cv2.imencode('.jpg', image)
+    if not ret:
+        return ''
+    return b64encode(jpg.tobytes()).decode()
+
+
 class Camera:
     def __init__(self) -> None:
         self.__cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
@@ -46,7 +59,7 @@ class Camera:
             self.__delay = 1 / self.__FPS
             self.__width = self.__cap.get(cv2.CAP_PROP_FRAME_WIDTH)
             self.__height = self.__cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            logging.info('Camera open successful')
+            log.info('Camera open successful')
         else:
             raise RuntimeError('Camera open fail')
 
@@ -92,3 +105,64 @@ class Camera:
 
     def delay(self):
         return self.__delay
+
+
+class Camera2(RepeatTimer):
+    def __init__(self):
+        self.__cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+        if not self.__cap.isOpened():
+            raise RuntimeError('Camera open fail')
+        log.info('Camera open successful')
+        self.__FPS = self.__cap.get(cv2.CAP_PROP_FPS)
+        self.__delay = 1 / self.__FPS
+        self.__width = self.__cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.__height = self.__cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.__ret = False
+        self.__image = None
+        self.lightness_text = " .:-=+*#%@"
+        self.light_lv = len(self.lightness_text) - 1
+        RepeatTimer.__init__(self, interval=0.)
+
+    def __str__(self):
+        s = 'FPS: %f  Delay: %f  Width: %f  Height: %f\n' % (self.__FPS, self.__delay, self.__width, self.__height)
+        if not self.__ret:
+            s += '**NO IMAGE**'
+            return s
+        s += '-' * (_ascii_w + 2) + '\n'
+        image = cv2.resize(self.__image, (_ascii_w, _ascii_h))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        for row in image:
+            s += '|'
+            for pixel in row:
+                s += self.lightness_text[round(pixel / 255 * self.light_lv)]
+            s += '|\n'
+        s += '-' * (_ascii_w + 2)
+        return s
+
+    def init_phase(self):
+        pass
+
+    def execute_phase(self):
+        pass
+
+    def close_phase(self):
+        self.__cap.release()
+        self.__ret = False
+        self.__image = None
+
+    def get(self):
+        return self.__ret, self.__image
+
+    def get_resolution(self):
+        return self.__width, self.__height
+
+    def set_resolution(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def reset(self):
+        self.__cap.set(cv2.CAP_PROP_FRAME_WIDTH, _width)
+        self.__cap.set(cv2.CAP_PROP_FRAME_HEIGHT, _height)
+
+    def get_b64image(self, image):
+        return encode_image_to_b64(image, (self.__width, self.__height))
