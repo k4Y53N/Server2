215ded1ad7aabcf06ceb7ef6c535f8689713bd9b
k4Y53N <ks93041@gmail.com>
2022-02-28 14:08:47 +0800

    linux shell printer test

--- src/ShellPrinter.py
+++ src/ShellPrinter.py
@@ -28,7 +28,7 @@ class Printer(RepeatTimer):
             print(self.get_cpu_usage())
             print(self.get_memory_usage())
         for obj in self.objs:
-            print('033J%s' % obj)
+            print('\033[J%s' % obj)
 
     @staticmethod
     def clean_screen():
