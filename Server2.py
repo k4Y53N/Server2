430b6c6118299b2b65d1736849e0ee7e5f652829
k4Y53N <ks93041@gmail.com>
2022-03-16 22:02:22 +0800

    complete main server func

--- Server2.py
+++ Server2.py
@@ -1,27 +1,25 @@
 import logging as log
-from threading import Thread
-from src.Camera import Camera
+from threading import Thread, Lock
 from src.Connection import Connection
-from src.Detector import Detector
 from src.Monitor import Monitor
 from src.utils.Commands import FRAME, SYS_INFO, CONFIGS, CONFIG
 from src.utils.util import get_hostname
-from typing import Tuple
-
-
-class SystemInfo:
-    def __init__(self):
-        pass
+from src.RepeatTimer import RepeatTimer
+from src.Streamer import Streamer
+from pathlib import Path
+from typing import Tuple, Union
+from time import sleep
 
 
 class Server(Thread):
-    def __init__(self) -> None:
+    def __init__(self, configs_path: Path, port=0, exc_info=False) -> None:
         Thread.__init__(self)
-        self.__is_running = True
-        self.__detector = Detector()
-        self.__camera = Camera()
-        self.__connection = Connection(get_hostname())
+        self.is_running = True
+        self.__connection = Connection(get_hostname(), port, exc_info=exc_info)
         self.__monitor = Monitor()
+        self.streamer = Streamer(configs_path)
+        self.serve_address: Union[Tuple[str, int], None] = None
+        self.lock = Lock()
         self.__func_map = {
             'RESET': self.__reset,
             'GET_SYS_INFO': self.__get_sys_info,
@@ -33,91 +31,77 @@ class Server(Thread):
             'SET_QUALITY': self.__set_quality,
             'MOV': self.__move
         }
-        self.sys_info = SYS_INFO.copy()
+        self.exc_info = exc_info
+        self.is_running = True
         self.__event_thread = Thread(target=self.__event_loop, daemon=True)
-        self.__stream_thread = Thread(target=self.__streaming, daemon=True)
+        self.stream_repeat_timer = RepeatTimer(target=self.streaming, interval=0)
 
     def run(self):
         self.__monitor.set_row_string(0, '%s:%d' % self.__connection.get_server_address())
         self.__connection.start()
         self.__monitor.start()
         self.__event_thread.start()
-        self.__stream_thread.start()
         self.__connection.join()
         self.__monitor.join()
         self.__event_thread.join()
-        self.__stream_thread.join()
 
     def __event_loop(self):
-        serve_address = self.__connection.get_client_address()
-        while self.__connection.is_alive() and self.__is_running:
-            command, address = self.__connection.get()
-            if address != serve_address:
-                self.__reset(address)
-                serve_address = address
-            if not command:
-                continue
-            else:
+        try:
+            while self.__connection.is_connect() and self.is_running:
+                command, address = self.__connection.get()
+                if address != self.serve_address:
+                    self.__reset(address)
+                if not command:
+                    continue
                 self.__event(command, address)
-        self.close()
+        except Exception as E:
+            log.error(f'Event loop was interrupt {E.__class__.__name__}', exc_info=True)
+        finally:
+            self.close()
 
     def __event(self, command: dict, address: tuple):
-        command_key = command.get('CMD', None)
-        if command_key not in self.__func_map.keys():
+        try:
+            command_key = command.get('CMD', None)
+            if command_key not in self.__func_map.keys():
+                return
+            ret = self.__func_map[command_key](command)
+            if ret:
+                self.__connection.put(ret, address)
+        except Exception as E:
+            log.error(f'illegal command {command} cause {E.__class__.__name__}', exc_info=True)
+
+    def streaming(self, interval=0.2):
+        acquired = self.lock.acquire(False)
+        address = self.serve_address
+        if not acquired or not address:
+            sleep(interval)
             return
-        ret = self.__func_map[command_key](command)
-        if ret:
-            self.__connection.put(ret, address)
-
-    def __streaming(self):
-        while self.__connection.is_alive() and self.__is_running:
-            pass
-            # address = self.__connection.get_client_address()
-            # # if (not address) or (not self.__camera.is_client_stream()):
-            # #     sleep(1)
-            # #     continue
-            #
-            # grab, image = self.__camera.get()
-            # if (not grab) or (not image):
-            #     sleep(1)
-            #     continue
-            #
-            # if self.__detector.is_client_infer():
-            #     ret = self.__vid_encode_infer(image)
-            #     self.__connection.put(ret, address)
-            # else:
-            #     ret = self.__vid_encode(image)
-            #     self.__connection.put(ret, address)
-
-    def __vid_encode_infer(self, image) -> dict:
-        frame = FRAME.copy()
-        encode_thread = self.__camera.get_encode_thread(frame, 'IMAGE', image)
-        infer_thread = self.__detector.infer_thread(frame, 'BBOX', image)
-        encode_thread.start()
-        infer_thread.start()
-        encode_thread.join()
-        infer_thread.join()
-        return frame
-
-    def __vid_encode(self, image) -> dict:
-        frame = FRAME.copy()
-        self.__camera.encode(frame, 'IMAGE', image)
-        return frame
+        try:
+            b64image, boxes, boxes_class = self.streamer.get()
+            if not b64image:
+                sleep(interval)
+                return
+            frame = FRAME.copy()
+            frame['IMAGE'] = b64image
+            frame['BBOX'] = boxes
+            frame['CLASS'] = boxes_class
+            self.__connection.put(frame, address)
+        finally:
+            self.lock.release()
 
     def __reset(self, client_address):
-        self.sys_info = SYS_INFO.copy()
+        with self.lock:
+            self.serve_address = client_address
+            self.streamer.reset()
+
         client_address = '%s:%d' % client_address if client_address else None
         self.__monitor.set_row_string(1, client_address)
-        self.__camera.reset()
-        self.__detector.reset()
-        self.sys_info['CAMERA_WIDTH'], self.sys_info['CAMERA_HEIGHT'] = self.__camera.get_resolution()
 
     def close(self):
-        self.__is_running = False
-        self.__camera.close()
+        self.is_running = False
         self.__connection.close()
-        self.__detector.close()
         self.__monitor.close()
+        self.stream_repeat_timer.close()
 
     def __exit(self):
         pass
@@ -126,48 +110,43 @@ class Server(Thread):
         pass
 
     def __get_sys_info(self, command: dict, *args, **kwargs) -> dict:
-        return self.sys_info
+        sys_info = SYS_INFO.copy()
+        sys_info['IS_INFER'] = self.streamer.is_infer()
+        sys_info['IS_STREAM'] = self.streamer.is_stream()
+        sys_info['CAMERA_WIDTH'], SYS_INFO['CAMERA_HEIGHT'] = self.streamer.get_quality()
+        return sys_info
 
     def __get_configs(self, command: dict, *args, **kwargs) -> dict:
         configs = CONFIGS.copy()
-        configs.update(self.__detector.get_configs())
+        configs['CONFIGS'] = self.streamer.get_configs()
         return configs
 
     def __get_config(self, command: dict, *args, **kwargs) -> dict:
         config = CONFIG.copy()
-        config.update(self.__detector.get_config())
+        config.update(self.streamer.get_config())
         return config
 
     def __set_stream(self, command: dict, *args, **kwargs) -> None:
-        pass
-        # set_stream = command.get('STREAM', None)
-        # if not set_stream:
-        #     return None
-        # self.__camera.set_stream(bool(set_stream))
-        # if self.__camera.is_client_stream():
-        #     if self.__stream_thread.is_alive():
-        #         return
-        #     try:
-        #         self.__stream_thread.start()
-        #     except RuntimeError:
-        #         self.__stream_thread = Thread(target=self.__streaming, daemon=True)
-        #         self.__stream_thread.start()
+        is_stream = bool(command.get('STREAM'))
+        self.streamer.set_stream(is_stream)
 
     def __set_config(self, command: dict, *args, **kwargs) -> None:
-        self.__detector.set_config(config=command)
-        return None
+        config_name = command.get('CONFIG')
+        self.streamer.set_config(config_name)
 
     def __set_infer(self, command: dict, *args, **kwargs) -> None:
-        self.__detector.set_infer(command)
-        return None
+        is_infer = bool(command.get('INFER'))
+        self.streamer.set_infer(is_infer)
 
     def __set_quality(self, command: dict, *args, **kwargs) -> None:
-        self.__camera.set_quality(1920, 1080)
-        return None
+        width = int(command.get('WIDTH', 0))
+        height = int(command.get('HEIGHT', 0))
+        if 100 < width < 4196 or 100 < height < 4196:
+            return
+        self.streamer.set_quality(width, height)
 
-    def __move(self, command: dict, address: Tuple[str, int], *args, **kwargs) -> None:
+    def __move(self, command: dict, *args, **kwargs) -> None:
         pass
-        return None
 
 
 if __name__ == '__main__':
@@ -176,7 +155,8 @@ if __name__ == '__main__':
         datefmt='%Y/%m/%d %H:%M:%S',
         level=log.INFO
     )
-    server = Server()
+    configs_path = Path('configs')
+    server = Server(configs_path, exc_info=True)
 
     try:
         server.run()
