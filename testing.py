a9e98d008a06ad3e6a0f19e5f845d136c7ab6510
k4Y53N <ks93041@gmail.com>
2022-03-26 21:33:53 +0800

    add memory limit

--- testing.py
+++ testing.py
@@ -2,6 +2,20 @@ from pathlib import Path
 from src.Detector import Detector
 import cv2
 from timeit import timeit
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.set_logical_device_configuration(
+            gpus[0],
+            [tf.config.LogicalDeviceConfiguration(memory_limit=2048)])
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
 
 p = Path('configs/')
 d = Detector(p)
