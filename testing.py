be6bf55c012996eed8dd738d4854f2d543047643
k4Y53N <ks93041@gmail.com>
2022-03-27 03:00:02 +0800

    env test

--- testing.py
+++ testing.py
@@ -9,8 +9,8 @@ image = cv2.imread('person.jpg')
 d.score_threshold = 0.5
 d.iou_threshold = 0.5
 d.load_model('person-320-noPre.json')
-d.model.save_weights('person-320-noPre.h5')
-d.model.save('checkpoints/person-320-noPre')
+# d.model.save_weights('person-320-noPre.h5')
+# d.model.save('checkpoints/person-320-noPre')
 result = d.detect(image)
 
 print(timeit('d.detect(image)', globals=globals(), number=10))
