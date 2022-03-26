db27e1d8d33426742421f10ef767cb297a0a2eff
k4Y53N <ks93041@gmail.com>
2022-03-26 22:34:30 +0800

    env test

--- testing.py
+++ testing.py
@@ -23,7 +23,7 @@ d = Detector(p)
 image = cv2.imread('person.jpg')
 d.score_threshold = 0.5
 d.iou_threshold = 0.5
-d.load_model('yolov4-tiny-416.json')
+d.load_model('yolov4-416.json')
 result = d.detect(image)
 
 print(timeit('d.detect(image)', globals=globals(), number=10))
