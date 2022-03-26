e72182e24697a2d346049b470dddc8d0bc39306a
k4Y53N <ks93041@gmail.com>
2022-03-26 17:08:05 +0800

    for model testing

---
+++ testing.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from src.Detector import Detector
+import cv2
+from timeit import timeit
+
+p = Path('configs/')
+d = Detector(p)
+image = cv2.imread('person.jpg')
+d.score_threshold = 0.5
+d.iou_threshold = 0.5
+d.load_model('yolov4-tiny-416.json')
+result = d.detect(image)
+
+print(timeit('d.detect(image)', globals=None))
