1c51c0d3d75423162b3518c12ab7bb8c3224230d
k4Y53N <ks93041@gmail.com>
2022-04-12 07:37:22 +0800

    using builder to pack vars

--- src/Detector/Detector.py
+++ src/Detector/Detector.py
@@ -1,10 +1,18 @@
 from .RemoteDetector import RemoteDetector
 from .LocalDetector import LocalDetector
-from pathlib import Path
+
+
+class DetectorBuilder:
+    def __init__(self):
+        self.yolo_configs_dir = ''
+        self.is_show_exc_info = True
+        self.is_local_detector = True
+        self.remote_detector_ip = 'localhost'
+        self.remote_detector_port = 0
 
 
 class Detector:
-    def __new__(cls, configs_path: Path, is_local: bool, ip='localhost', port=0, *args, **kwargs):
-        if is_local:
-            return LocalDetector(configs_path)
-        return RemoteDetector(ip, port)
+    def __new__(cls, builder: DetectorBuilder):
+        if builder.is_local_detector:
+            return LocalDetector(builder.yolo_configs_dir, is_show_exc_info=True)
+        return RemoteDetector(builder.remote_detector_ip, builder.remote_detector_port, builder.is_show_exc_info)
